b5a79b8c65d7f9b3919025cb728dc03be39dcffb
Alec Skelly <skelly@spamcop.net>
2012-11-01 09:30:54 -0700

    Added copyright and GPL notices to files and included a copy of the GPL.

--- SANEWinDS/FormSANEHostWizard.vb
+++ SANEWinDS/FormSANEHostWizard.vb
@@ -1,4 +1,22 @@
-﻿Public Class FormSANEHostWizard
+﻿'
+'   Copyright 2011, 2012 Alec Skelly
+'
+'   This file is part of SANEWinDS.
+'
+'   SANEWinDS is free software: you can redistribute it and/or modify
+'   it under the terms of the GNU General Public License as published by
+'   the Free Software Foundation, either version 3 of the License, or
+'   (at your option) any later version.
+'
+'   SANEWinDS is distributed in the hope that it will be useful,
+'   but WITHOUT ANY WARRANTY; without even the implied warranty of
+'   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'   GNU General Public License for more details.
+'
+'   You should have received a copy of the GNU General Public License
+'   along with SANEWinDS.  If not, see <http://www.gnu.org/licenses/>.
+'
+Public Class FormSANEHostWizard
     Private Sub ButtonNext_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonNext.Click
         If Me.PanelHost.Visible Then
             If Me.ComboBoxHost.Text IsNot Nothing AndAlso Me.ComboBoxHost.Text.Length Then

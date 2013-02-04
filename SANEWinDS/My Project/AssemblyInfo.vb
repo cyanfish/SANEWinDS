6a4808b69ace5f1fe913f42737eb374e3ba00969
Alec Skelly <skelly@spamcop.net>
2013-02-03 23:48:11 -0800

    Updated version to 0.8.4782.

--- SANEWinDS/My Project/AssemblyInfo.vb
+++ SANEWinDS/My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("0.8.*")> 
-<Assembly: AssemblyFileVersion("0.8.4776")> 
+<Assembly: AssemblyFileVersion("0.8.4782")> 

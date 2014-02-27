f4a077367d3c60452f0474b4eb18d24697b36b32
Alec Skelly <skelly@spamcop.net>
2014-02-27 00:25:06 -0800

    Bumped version to 0.9.5171 and updated installers.

--- SANEWinDS/My Project/AssemblyInfo.vb
+++ SANEWinDS/My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("0.9.*")> 
-<Assembly: AssemblyFileVersion("0.9.5144")> 
+<Assembly: AssemblyFileVersion("0.9.5171")> 

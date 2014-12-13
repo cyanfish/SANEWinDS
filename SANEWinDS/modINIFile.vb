f0f739ce51ba234ed249e9c6bbea4a2c42e983a3
Alec Skelly <OBloodyL@gmail.com>
2014-12-12 23:48:24 -0800

    Updated license statment in modINIFile.  Upgraded NLog in SANEWin.

--- SANEWinDS/modINIFile.vb
+++ SANEWinDS/modINIFile.vb
@@ -1,21 +1,21 @@
 ﻿'
-' Copyright 2013 Alec Skelly
+'   Copyright 2013-2014 Alec Skelly
 '
-' This file is part of VB5250, a telnet client implementing IBM's 5250 protocol.
+'   This file is part of SANEWinDS.
 '
-' VB5250 is free software: you can redistribute it and/or modify
-' it under the terms of the GNU General Public License as published by
-' the Free Software Foundation, either version 3 of the License, or
-' (at your option) any later version.
+'   SANEWinDS is free software: you can redistribute it and/or modify
+'   it under the terms of the GNU General Public License as published by
+'   the Free Software Foundation, either version 3 of the License, or
+'   (at your option) any later version.
 '
-' VB5250 is distributed in the hope that it will be useful,
-' but WITHOUT ANY WARRANTY; without even the implied warranty of
-' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-' GNU General Public License for more details.
+'   SANEWinDS is distributed in the hope that it will be useful,
+'   but WITHOUT ANY WARRANTY; without even the implied warranty of
+'   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'   GNU General Public License for more details.
 '
-' You should have received a copy of the GNU General Public License
-' along with VB5250. If not, see <http://www.gnu.org/licenses/>.
-' 
+'   You should have received a copy of the GNU General Public License
+'   along with SANEWinDS.  If not, see <http://www.gnu.org/licenses/>.
+'
 Module modINIFile
     Private Logger As NLog.Logger = NLog.LogManager.GetCurrentClassLogger
 

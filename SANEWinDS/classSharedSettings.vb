0dcaf5e550183c4fef0969bd2130446011a2ab38
Alec Skelly <skelly@spamcop.net>
2015-05-22 08:52:10 -0700

    Removed Alpha software warning displayed after an upgrade.

--- SANEWinDS/classSharedSettings.vb
+++ SANEWinDS/classSharedSettings.vb
@@ -192,10 +192,7 @@ Public Class SharedSettings
         If Not SuppressWarning Then
             Dim App_Version As String = ReadIni(UserSettingsFileName, "General", "Version")
             If App_Version <> GetType(SANE_API).Assembly.GetName.Version.ToString Then
-                Dim r As MsgBoxResult = MsgBox("Please note that SANEWinDS is in the alpha stage of development.  Expect numerous bugs.  " _
-                    & "When you encounter a bug, please notify the authors by opening a ticket or posting in the forum " _
-                    & "at http://sourceforge.net/projects/sanewinds/.", MsgBoxStyle.Exclamation + MsgBoxStyle.OkOnly, "Alpha Software Alert")
-                r = MsgBox("SANEWinDS is hosted by SourceForge.  If you downloaded it from any other site you probably don't have the most recent version.  " _
+                Dim r As MsgBoxResult = MsgBox("SANEWinDS is hosted by SourceForge.  If you downloaded it from any other site you probably don't have the most recent version.  " _
                     & "The current version is available at http://sourceforge.net/projects/sanewinds/ along with configuration instructions and a forum for " _
                     & "bug reporting, feature requests, and backend.ini contributions.", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "SANEWinDS is hosted by SourceForge!")
             End If
